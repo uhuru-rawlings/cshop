6a6340b73ee5c068fe09e60de2ae765266d90232
uhururawlings40 <uhururawlings40@gmail.com>
2022-05-15 12:52:09 +0300

    cshop folder stracture

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,28 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './register/register.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { CartComponent } from './cart/cart.component';
+import { ItemsComponent } from './items/items.component';
+import { HistoryComponent } from './history/history.component';
+import { CheckoutComponent } from './checkout/checkout.component';
+import { DetailsComponent } from './details/details.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    HomepageComponent,
+    CartComponent,
+    ItemsComponent,
+    HistoryComponent,
+    CheckoutComponent,
+    DetailsComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
