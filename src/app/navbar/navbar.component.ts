bab0c381a5dd351091d39415f1059416e23a079f
uhururawlings40 <uhururawlings40@gmail.com>
2022-05-15 23:54:50 +0300

    added toogle login logout button

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-navbar',
@@ -6,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
-
-  constructor() { }
+  user:any = ''
+  constructor(private cookie:CookieService) { }
 
   ngOnInit(): void {
+    this.getUser()
   }
-
+  getUser(){
+    let usern = this.cookie.get("userd")
+    if(usern){
+     this.user = usern
+    }
+  }
 }
