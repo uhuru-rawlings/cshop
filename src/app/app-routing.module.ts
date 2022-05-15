0add81876b7ca8df8609c26c5070a69f19b77102
uhururawlings40 <uhururawlings40@gmail.com>
2022-05-15 13:59:46 +0300

    added routing to angular app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 
 import { RegisterComponent } from './register/register.component';
@@ -9,10 +10,33 @@ import { HistoryComponent } from './history/history.component';
 import { CheckoutComponent } from './checkout/checkout.component';
 import { DetailsComponent } from './details/details.component';
 
-const routes: Routes = [];
 
+const routes: Routes = [
+  {path: '',component:RegisterComponent},
+  {path: 'cshop',component:HomepageComponent},
+  {path: 'cshop/cart',component:CartComponent},
+  {path: 'cshop/items',component:ItemsComponent},
+  {path: 'cshop/history',component:HistoryComponent},
+  {path: 'cshop/checkout',component:CheckoutComponent},
+  {path: 'cshop/details',component:DetailsComponent},
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const navLinks = [RegisterComponent,HomepageComponent,CartComponent,ItemsComponent,HistoryComponent,CheckoutComponent,DetailsComponent]
+
+// import { NgModule } from '@angular/core';
+//
+
+
+
+
+
+// @NgModule({
+//   imports: [RouterModule.forRoot(routes)],
+//   exports: [RouterModule]
+// })
+// export class AppRoutingModule { }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,34 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from './app-routing.module';
+import { RouterModule, Routes } from '@angular/router'
 
 import { AppComponent } from './app.component';
-import { RegisterComponent } from './register/register.component';
-import { HomepageComponent } from './homepage/homepage.component';
-import { CartComponent } from './cart/cart.component';
-import { ItemsComponent } from './items/items.component';
-import { HistoryComponent } from './history/history.component';
-import { CheckoutComponent } from './checkout/checkout.component';
-import { DetailsComponent } from './details/details.component';
+import { AppRoutingModule,navLinks } from './app-routing.module';
+
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RegisterComponent,
-    HomepageComponent,
-    CartComponent,
-    ItemsComponent,
-    HistoryComponent,
-    CheckoutComponent,
-    DetailsComponent,
+    navLinks,
     NavbarComponent,
     FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
