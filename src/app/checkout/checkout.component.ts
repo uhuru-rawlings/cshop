60decd39f8abd1c047259b1fa141f194a99ba157
uhururawlings40 <uhururawlings40@gmail.com>
2022-05-15 23:39:11 +0300

    added data binding to checkout form

--- src/app/checkout/checkout.component.ts
+++ src/app/checkout/checkout.component.ts
@@ -6,7 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./checkout.component.css']
 })
 export class CheckoutComponent implements OnInit {
-
+  pickupdate:any = ''
+  pickupcity:any = ''
+  adress:any = ''
+  idnumber:any = ''
+  recipientcontact:any = ''
   constructor() { }
 
   ngOnInit(): void {
