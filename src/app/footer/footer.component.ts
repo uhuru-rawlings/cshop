fa6f01781c2668cff93ca571c4117da919afddb3
uhururawlings40 <uhururawlings40@gmail.com>
2022-05-15 22:36:45 +0300

    added validate input for email'

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-footer',
@@ -6,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
-
-  constructor() { }
+  emailadress:any = ''
+  constructor(private toastr:ToastrService) { }
 
   ngOnInit(): void {
   }
-
+  
+  validateNewslater(){
+    if(this.emailadress === ''){
+      this.toastr.error("Please provide email adress.")
+      return
+    }
+  }
 }
