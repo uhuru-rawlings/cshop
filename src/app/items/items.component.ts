a633c82ed6ad2b75b52d4b226715d020f0fcc729
uhururawlings40 <uhururawlings40@gmail.com>
2022-05-15 23:07:26 +0300

    added checkout page

--- src/app/items/items.component.ts
+++ src/app/items/items.component.ts
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./items.component.css']
 })
 export class ItemsComponent implements OnInit {
-
+  selectitem:any = ''
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  
+  onChangeSort(){
+    if(this.selectitem !== ''){
+      // get sorted.
+    }
+  }
 }
