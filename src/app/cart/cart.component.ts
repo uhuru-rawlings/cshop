6b11cb2f04440327b8a65733c65cdf4d7a1e69d2
uhururawlings40 <uhururawlings40@gmail.com>
2022-05-17 18:30:28 +0300

    added table for cart items

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-cart',
@@ -6,10 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cart.component.css']
 })
 export class CartComponent implements OnInit {
-
-  constructor() { }
+  items:any;
+  constructor(private cookie:CookieService, private toastr:ToastrService) { }
 
   ngOnInit(): void {
   }
-
+  
+  setCartItems(){
+    let item_cart = this.cookie.get("cartItems")
+    if(item_cart){
+      this.items = JSON.parse(item_cart)
+    }else{
+      this.items = "noitems"
+    }
+  }
 }
