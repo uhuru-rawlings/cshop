71a49d6cbd9cda627703c5c235765736f1a6d178
uhururawlings40 <uhururawlings40@gmail.com>
2022-05-17 11:32:30 +0300

    added getitems service

--- src/app/services/items.service.ts
+++ src/app/services/items.service.ts
@@ -1,9 +1,15 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ItemsService {
+  url:string = 'http://127.0.0.1:8000/cart/clothes/'
+  constructor(private http:HttpClient) { }
 
-  constructor() { }
+  getitems():Observable<any>{
+    return this.http.get(this.url)
+  }
 }
